309eb470b0c2fea7c5eef98cbc80acc4e5fed1f7
Janas-Z <234675034+Janas-Z@users.noreply.github.com>
2026-03-25 20:19:52 +0100

    Feat: Dodanie bazy danych in-memory do przechowywania stanu systemu

---
+++ Repositories/InMemoryDatabase.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp2.Domain;
+
+namespace ConsoleApp2.Repositories;
+
+public class InMemoryDatabase
+{
+    public List<User> Users { get; } = new();
+    public List<Equipment> Equipments { get; } = new();
+    public List<Rental> Rentals { get; } = new();
+}
