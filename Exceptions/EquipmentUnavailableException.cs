1baf25fcd9374970de0e0001c77d921b884a68cb
Janas-Z <234675034+Janas-Z@users.noreply.github.com>
2026-03-25 20:19:09 +0100

    Feat: Dodanie niestandardowych wyjątków biznesowych dla domeny

---
+++ Exceptions/EquipmentUnavailableException.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp2.Exceptions;
+
+public class EquipmentUnavailableException : Exception
+{
+    public EquipmentUnavailableException(string message) : base(message)
+    {
+    }
+}
---
+++ Exceptions/RentalLimitExceededException.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp2.Exceptions;
+
+public class RentalLimitExceededException : Exception
+{
+    public RentalLimitExceededException(string message) : base(message)
+    {
+    }
+}
